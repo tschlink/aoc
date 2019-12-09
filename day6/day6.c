44ca72df9a4c3c74b5390140e28292f5fb1f76af
tom <schlink.tom@hotmail.com>
2019-12-09 01:47:33 +0100

    Start implementing orbit tree algorithms for day 6

--- day6/day6.c
+++ day6/day6.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <sys/types.h>
 
+char *strdup(const char *s);
+
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 typedef struct Orbit_T
@@ -11,6 +13,14 @@ typedef struct Orbit_T
     char* orbitee;
 } Orbit;
 
+typedef struct Orbit_Tree_T
+{
+    char *planet;
+    struct Orbit_Tree_T *children;
+    int child_length;
+    struct Orbit_Tree_T *parent;
+} Orbit_Tree;
+
 void free_string_array(char **array)
 {
     for(int i = 0; *(array + i); i++)
@@ -18,6 +28,28 @@ void free_string_array(char **array)
         free(*(array + i));
     }
     free(array);
+}
+
+int rtrim(char *str)
+{
+    if(str == NULL) return 0;
+    int trimmable_chars = 0;
+    int length = strlen(str);
+
+    if(length == 0) return 0;
+    if(length == 1 && *str == '\0') return 0;
+
+    for(int i = length - 1; i > - 1; i--)
+    {
+        char current = str[i];
+        if(current == '\0' || current == ' ' || current == '\n' || current == '\t') trimmable_chars++;
+        else
+        {
+            str[i+1] = '\0';
+            break;
+        }
+    }
+    return trimmable_chars; // -1 to ignore trailing null byte. TODO this didn't work as expected, more debugging!
 }
 
 char** split(char *str, const char delim)
@@ -51,6 +83,7 @@ char** split(char *str, const char delim)
             *(result+idx) = malloc(sizeof(char) * strlen(token));
             strcpy(*(result + idx), token);
             token = strtok(0, delimiter);
+            rtrim(token);
             idx++;
         }
         *(result + idx) = 0;
@@ -74,8 +107,8 @@ Orbit* get_orbits(const char *path, int *orbit_number)
    int i = 0;
     while((getline(&line, &len, f)) != -1) {
         char** orb = split(line, ')');
-        strcpy(tmp[i].center, orb[0]);
-        strcpy(tmp[i].orbitee, orb[1]);
+        tmp[i].center = strdup(orb[0]);
+        tmp[i].orbitee = strdup(orb[1]);
         free_string_array(orb);
         i++;
     }
@@ -91,9 +124,42 @@ Orbit* get_orbits(const char *path, int *orbit_number)
     return orbits;
 }
 
+Orbit_Tree* createNode(char *str)
+{
+    Orbit_Tree *tree = calloc(1, sizeof(Orbit_Tree));
+    tree->planet = strdup(str);
+    tree->child_length = 0;
+    return tree;
+}
+
+void addChild(Orbit_Tree tree, char *str, Orbit orbit)
+{
+}
+
+Orbit_Tree* findNode(Orbit_Tree *root, char* str)
+{
+    if(root == NULL) return NULL;
+
+    if(0 == strcmp(root->planet, str)) return root;
+
+    for(int i = 0; i < root->child_length; i++)
+    {
+        Orbit_Tree *tmp = findNode((root->children + i), str);
+        if(tmp != NULL) return tmp;
+    }
+    return NULL;
+}
+
 int main(void)
 {
     int orbit_nbr = 0;
     Orbit *orbits = get_orbits("input.txt", &orbit_nbr);
+
+    Orbit_Tree *root = createNode("COM");
+
+    for(int i = 0; i < orbit_nbr; i++)
+    {
+    }
+
     return 0;
 }
